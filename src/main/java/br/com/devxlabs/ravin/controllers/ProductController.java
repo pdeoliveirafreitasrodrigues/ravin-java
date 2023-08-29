1d2d8fd10f29b284a2c4973ee2db7ea16f3d54d5
Paulo Rodrigues <55416067+pdeoliveirafreitasrodrigues@users.noreply.github.com>
2023-08-29 20:11:28 -0300

    Update ProductController

--- src/main/java/br/com/devxlabs/ravin/controllers/ProductController.java
+++ src/main/java/br/com/devxlabs/ravin/controllers/ProductController.java
@@ -3,10 +3,7 @@ package br.com.devxlabs.ravin.controllers;
 import br.com.devxlabs.ravin.services.ProductService;
 import models.dtos.ProductDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +17,16 @@ public class ProductController {
     @GetMapping
     public List<ProductDTO> listAll(){
         return service.listAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public ProductDTO findById(@PathVariable Long id){
+        return service.findById(id);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void deleteById(@PathVariable Long id){
+        service.deleteById(id);
     }
 
 
--- src/main/java/br/com/devxlabs/ravin/services/ProductService.java
+++ src/main/java/br/com/devxlabs/ravin/services/ProductService.java
@@ -2,7 +2,9 @@ package br.com.devxlabs.ravin.services;
 
 import models.dtos.ProductDTO;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.sql.SQLOutput;
 import java.util.List;
 
 @Service
@@ -10,7 +12,16 @@ public class ProductService {
 
     public List<ProductDTO> listAll(){
 
-        return List.of(new ProductDTO());
+        return List.of(new ProductDTO(), new ProductDTO());
+    }
+
+    public ProductDTO findById(long id){
+        System.out.print(id);
+        return new ProductDTO();
+    }
+
+    public void deleteById(Long id){
+        System.out.println("Deletou o produto " + id);
     }
 
 }
